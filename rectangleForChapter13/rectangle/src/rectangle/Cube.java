0d4d9d86f2b97aa4deba692f38d3198b5c2b4918
Kevin Bell <bellKevin@pm.me>
2022-08-29 12:11:02 -0600

    Update Cube.java

--- rectangleForChapter13/rectangle/src/rectangle/Cube.java
+++ rectangleForChapter13/rectangle/src/rectangle/Cube.java
@@ -6,16 +6,16 @@
  **************************************************************************** */
 package rectangle;
 
-public class Cube extends Rectangle {
+public class Box extends Rectangle {
 
     public double height, volume;
 
-    public Cube() {
+    public Box() {
         super("unknown box", 1, 1);
         height = 1;
     } // end constructor
 
-    public Cube(String name, double length, double width, double height) {
+    public Box(String name, double length, double width, double height) {
         super("unknown box", 1, 1);
         this.height = height;
     } // end constructor
@@ -39,7 +39,7 @@ public class Cube extends Rectangle {
 
     @Override
     public String display() {
-        String result = "New object: \n" + this.name + ", " + "dimensions: "
+        String result = this.name + ", " + "dimensions: "
                 + getLength() + " x " + getWidth() + " x " + getHeight() + "\n";
         if (isCube()) {
             result += "This object is a cube \n";
@@ -47,7 +47,7 @@ public class Cube extends Rectangle {
             result += "This object is not a cube \n";
         } // end if/else condition
         result += "Volume: " + this.volume + "\n";
-        System.out.println(result);
+        //System.out.println(result);
         return result;
     } // end display method
-} // end Cube class
+} // end Box class
