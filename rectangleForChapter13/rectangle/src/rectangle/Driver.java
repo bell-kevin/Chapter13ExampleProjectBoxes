906f36035d49f61ef9cf441072585d48cc5d627f
Kevin Bell <bellKevin@pm.me>
2022-08-29 15:53:16 +0000

    Update Driver.java

--- rectangleForChapter13/rectangle/src/rectangle/Driver.java
+++ rectangleForChapter13/rectangle/src/rectangle/Driver.java
@@ -13,10 +13,12 @@ public class Driver {
     public static void main(String[] args) {
         System.out.println("Ch 6 Rectangle Project by Kevin Bell \n");
         Rectangle paper = new Rectangle();
-        ArrayList<Long> rectangle = new ArrayList<>();
-        ArrayList<Long> cube = new ArrayList<>();
-        Cube box = new Cube();
-        Rectangle square = new Rectangle();
+        ArrayList<Rectangle> rectangle = new ArrayList<>();
+        ArrayList<Cube> cube = new ArrayList<>();
+        Rectangle r1 = new Rectangle("Birthday card", 6.5, 6.5);
+        Rectangle r2 = new Rectangle();
+        r2.setWidth(8.5);
+        r2.setLength(11);
         double length = 0, width = 0, height = 0;
         paper.setLength(length);
         paper.setWidth(width);
